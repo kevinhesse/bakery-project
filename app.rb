638247ba36ca4439e2988171bb28c35227a99d3b
kevin <khesse92@gmail.com>
2018-04-25 14:58:23 -0400

    added layout and materailize form

--- app.rb
+++ app.rb
@@ -4,5 +4,27 @@ include SendGrid
 
 get '/' do 
     "hello world"
-    erb :layout
+    erb :index
 end
+
+get '/contact' do
+    erb :contact
+end
+
+post "/" do 
+    puts params.inspect
+    from = Email.new(email:'khesse92@gmail.com')
+    to = Email.new(email:params[:email])
+    subject = params[:first_name] + " " + params[:last_name]
+    content = Content.new(type: 'text/plain', value: params[:comments] )
+    mail = Mail.new(from, subject, to, content)
+    
+    sg = SendGrid::API.new(api_key:ENV['SENDGRID_API_KEY'])
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    # puts response.parsed_body
+    puts response.headers
+    redirect "/contact"
+    
+end 
